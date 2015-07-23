1e7e71122266b82f145152646565c46cd5d6b9c5
Miguel Ángel Criado Romero <macro.masek@gmail.com>
2015-07-23 17:41:54 +0200

    Initial Commit

---
+++ Assets/Scripts/NaiveSpawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NaiveSpawner : MonoBehaviour {
+
+	public Transform cubePrefab;
+	public Transform sphereSpawnerPosition;
+	public float sphereRadius;
+	public float timeBetweenSpawns = 1;
+	public float numSpawns = 10;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (SpawnCubes ());
+	}
+
+	IEnumerator SpawnCubes() {
+		while (true) {
+			for (int i = 0; i < numSpawns; i++) {
+				RandomSpawn();
+			}
+			yield return new WaitForSeconds(timeBetweenSpawns);
+		}
+	}
+
+	void RandomSpawn() {
+		Transform newInstance;
+		newInstance = Instantiate (cubePrefab, Random.onUnitSphere * sphereRadius, Random.rotation) as Transform;
+		MeshRenderer renderer = newInstance.GetComponent<MeshRenderer> ();
+		if (renderer) {
+			renderer.material.color = new Color (Random.value, Random.value, Random.value);
+		}
+	}
+
+
+}
